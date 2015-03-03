f9ec7a45ba932da9c753a11ebc6f4179190df4d1
Spencer Tipping <spencer@spencertipping.com>
2015-03-03 15:59:01 +0000

    Thinking about how to compile continuation graphs

--- src/lisp/graph.pl
+++ src/lisp/graph.pl
@@ -38,137 +38,37 @@
 # that is, if we have (k1 4) (k1 5) (k1 6) and (k2 a) (k2 b), the (co*)
 # continuation might see [4 a] [5 a] [6 a] or it might see [4 a] [4 b] [5 b],
 # etc. The queues of k1 and k2 are mutually unordered.
+#
+# Along with (co*) is (amb*), which forwards only the first continuation. That
+# is:
+#
+# > (amb* (λk1 ...)
+#         (λk2 ...)
+#         (λv (print v)))
+# > (k1 5)              # (print 5)
+# > (k2 4)              # nothing happens
+# > (k1 7)              # (print 7)
+#
+# It's important to deactivate all continuations except for the first because
+# the purpose of (amb*) is to express semantic ambivalence about the
+# implementation of a given computation; therefore, we still want just one
+# result despite providing two ways to calculate it.
+
+# Compiling this representation
+# The graph form is reduced to special nodes, each of which is one of the
+# following:
+#
+# (co*   f1 f2 ... k)
+# (amb*  f1 f2 ... k)
+# (call* f x1 x2 ... k)
+# (nth*  i f1 f2 ... k)         # used to encode (if) and (cond)
+# (fn*   form)                  # lambda function
+# (di*   n)                     # De Bruijn lambda argument reference
 
 {
 
-package ni::lisp;
+package ni::lisp::graph;
 
-sub fn_node      { ni::lisp::fn->new(@_) }
-sub co_node      { ni::lisp::co->new(@_) }
-sub if_node      { ni::lisp::if->new(@_) }
-sub apply_node   { ni::lisp::apply->new(@_) }
 
-sub global_node  { ni::lisp::global->new(@_) }
-sub value_node   { ni::lisp::value->new(@_) }
-sub bless_node   { ni::lisp::value->new(@_[1..$#_])->type($_[0]) }
-sub literal_node { ni::lisp::value->new->constant(@_) }
-
-}
-
-{
-
-package ni::lisp::node;
-
-sub then {
-  my ($self, $x) = @_;
-  push @{$$x{wait_for} //= []}, $self;
-  $self;
-}
-
-sub is_constant { 0 }
-
-our $gensym_id = 0;
-sub gensym { '__' . ($_[0] // 'gensym') . '_' . ++$gensym_id }
-
-sub defnodetype {
-  my ($t, $ctor, %methods) = @_;
-  @{"ni::lisp::${t}::ISA"} = qw/ ni::lisp::node /;
-  *{"ni::lisp::${t}::new"} = sub {
-    local $_;
-    my ($class, @args) = @_;
-    bless $ctor->(@args), $class;
-  };
-  *{"ni::lisp::${t}::$_"} = $methods{$_} for keys %methods;
-}
-
-defnodetype 'fn',
-sub { +{self_ref => $_[0],
-        formal   => $_[1],
-        body     => $_[2]} },
-{
-  compile => sub {
-    # Compile into a reified function within the language in question.
-    my ($self, $language) = @_;
-    my $gensym   = gensym 'fn';
-    my $v_gensym = gensym 'x';
-    $language->defn($gensym, $v_gensym, $$self{body});
-  },
-};
-
-defnodetype 'co',
-sub { [@_] },
-{
-  compile => sub {
-    # For now, compile in sequential order and return a reified list.
-    my ($self, $language) = @_;
-    my $gensym = gensym 'co';
-    $language->array($gensym, map $_->compile($language), @$self);
-  },
-};
-
-defnodetype 'if',
-sub { +{cond => $_[0], then => $_[1], else => $_[2]} },
-{
-  compile => sub {
-    my ($self, $language) = @_;
-    my $cond = $$self{cond}->compile($language);
-    $language->choose($cond, $$self{then}, $$self{else});
-  },
-};
-
-defnodetype 'apply',
-sub { +{f => $_[0], args => [@_[1..$#_]]} },
-{
-  compile => sub {
-    my ($self, $language) = @_;
-    my $f             = $$self{f};
-    my $compiled_args = co->new(@{$$self{args}})->compile($language);
-    if ($f->is_constant) {
-      $f = $$f{value};
-      die "cannot call a non-function $f" unless ref $f eq 'ni::lisp::fn';
-      my $inlined_body = $$f{body}->to_graph({''            => $$f{scope},
-                                              $$f{formal}   => $compiled_args,
-                                              $$f{self_ref} => $f});
-      $inlined_body->compile($language);
-    } else {
-      $language->apply($f->compile($language), $compiled_args);
-    }
-  },
-};
-
-defnodetype 'value',
-sub { +{value => $_[1], type => $_[0], constant => 0} },
-{
-  constant => sub {
-    my ($self, $type, $x) = @_;
-    $$self{constant} = 1;
-    $$self{type}     = $type;
-    $$self{value}    = $x;
-    $self;
-  },
-
-  type => sub {
-    my ($self, $t) = @_;
-    $$self{type} = $t;
-    $self;
-  },
-
-  value => sub {
-    my ($self) = @_;
-    $$self{value};
-  },
-
-  is_constant => sub {
-    my ($self) = @_;
-    $$self{constant};
-  },
-
-  compile => sub {
-    my ($self, $language) = @_;
-    $self->is_constant
-      ? $language->typed_constant($$self{type}, $$self{value})
-      : $$self{value}->compile($language);
-  },
-};
 
 }
