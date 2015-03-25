41c1698880e3b47f3ae8d13e8f48c847511867e9
Spencer Tipping <spencer@spencertipping.com>
2015-03-25 13:05:13 +0000

    More specification

--- src/boot-compiler.pl
+++ src/boot-compiler.pl
@@ -57,9 +57,7 @@
 # which will push a reference to itself) because lists are immutable. That
 # said, it won't matter because by the time any code hits a backend the return
 # list will have been erased.
-
-package nb;
-
+#
 # Special forms.
 # This is more interesting than it sounds because ni-lisp can't refer to any
 # concatenative functions directly (due to the difference in calling
@@ -70,25 +68,74 @@ package nb;
 #
 # Having said all of that, here's the list of special forms ni-lisp recognizes:
 #
-# (fn* formal body)             # unary formal and return (!)
+# (fn* self formal body)        # unary formal and return (!)
 # (co* f1 f2 ... fN)            # returns a list of ((f1) (f2) ... (fN))
 # (amb* f1 f2 ... fN)           # returns one of (f1), (f2), ..., or (fN)
 # (nth* i x0 x1 ... xN)         # returns x_i, segfaults if i out of bounds
-# (do* x1 x2 ... xN)            # do x1, x2, ..., xN sequentially, return xN
+# (do* f1 f2 ... fN)            # do (f1), then (f2), ..., then return (fN)
 #
 # We also allocate special forms to do stuff with continuations and resolvers:
 #
 # (def* name value)             # updates the resolver
-# (call/cc* f)                  # calls f with an inspectable continuation
-# (set/cc* k)                   # uses a continuation
+# (call/cc* f)                  # calls f with the current continuation
 #
-# Continuations behave a little differently than they do in Scheme in that you
-# can't just invoke one as a function. However, despite this limitation they
-# have the same performance and space characteristics (i.e. amortized constant
-# time, and minimal amortized space).
+# Continuations are inspectable lists and ni-lisp functions, but they contain
+# some wrapper code to convert from the concatenative notion to functions that
+# behave like the ones in Scheme. (That is, ni-lisp isn't itself CPS-converted;
+# its continuations are internally encoded as concatenative continuations,
+# which makes them fully serializable.)
+#
+# If you use call/cc*, the continuation you receive will always have the
+# following form:
+#
+# ((tail-stack)         # self-quoting list form
+#  (data-stack)         # self-quoting list form
+#  'resolver            # self-quoting form of some kind (probably a hash)
+#  setcc)
+#
+# This means you can reliably inspect and transform the continuation (which is
+# how ni-lisp implements low-level fexprs, though you shouldn't use them).
+# Generating and using a continuation is often much faster than inspecting it.
 #
 # The resolver's role in ni-lisp is a little strange. ni-lisp uses a wrapped
-# calling convention, so it requires a separate namespace. This is encoded by
+# calling convention, so it requires a separate namespace. This is done by
 # having all ni-lisp names internally prefixed with :. Typically you wouldn't
 # encounter this prefix unless you were printing a function as a list or some
 # such.
+#
+# Interfacing functions.
+# In addition to the above special forms, we need a set of builtin functions so
+# we can do stuff with data structures. These are intentionally named after
+# functions in Clojure and have similar semantics:
+#
+# (symbol? x)
+# (string? x)
+# (number? x)
+# (nil? xs)
+# (count xs)
+#
+# (cons x xs)
+# (first xs)
+# (rest xs)
+# (list x y z ...)
+# (list? x)
+#
+# (vector a b c ...)
+# (vec xs)
+# (nth xs i)
+# (vec? x)
+#
+# (hash-map k1 v1 k2 v2 ...)
+# (assoc h k1 v1 k2 v2 ...)
+# (dissoc h k1 k2 k3 ...)
+# (keys h)
+# (vals h)
+# (contains? h k)
+# (get h k)
+# (get h k not-found)
+# (map? x)
+#
+# Other data structures are built on top of these "primitives" to (mostly)
+# achieve parity with Clojure.
+
+package nb;
