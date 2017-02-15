06c4e0376fb39e5d50251cde7687c642abb536f1
Spencer Tipping <spencer.tipping@gmail.com>
2017-02-14 20:32:50 -0700

    Starting the fabric module

---
+++ src/fabric.pl
@@ -0,0 +1,8 @@
+ni('ni:/lib/doc')->new('/fabric')
+  ->description(
+    q[Abstractions to bridge the gaps between separate machines and processes.
+      This module is designed to make it appear as though all resources are
+      local, or at least can be referred to locally -- even when they belong to
+      an external process (e.g. a Hadoop mapper) or another machine (e.g. a
+      file over SSH). If we can bidirectionally communicate with a remote ni
+      instance, then we can see its resources.]);
