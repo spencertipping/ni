688b1867ab062bafaad301578f890404820c8753
Spencer Tipping <spencer.tipping@gmail.com>
2017-02-15 05:51:48 -0700

    Rewrite of UNIX library, now called IO

---
+++ src/io.pl
@@ -0,0 +1,16 @@
+ni('ni:/lib/doc')->new('/io')
+  ->description(
+    q[An implementation of IO in terms of system-level FDs. We need this for a
+      few reasons, three of them being that (1) old versions of Perl don't
+      correctly handle interrupted system calls, (2) we want tighter control
+      over which FDs are closed at what times, and (3) we want to be able to
+      "unread" things -- push back against the read buffer (or use a custom
+      read format in general).]);
+
+ni->extend("src/io/$_") for
+  qw/ object
+      cat
+      file
+      fd
+      pid
+      str /;
