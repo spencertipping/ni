9387a3784158ce2fd1998935787d9db4948ee33f
Spencer Tipping <spencer.tipping@gmail.com>
2017-02-16 20:48:45 -0700

    More complete definition of transfers

--- src/semantic.pl
+++ src/semantic.pl
@@ -3,3 +3,7 @@ ni('ni:/lib/doc')->new('/semantic')
     q[Opportunities to assign real-world semantics to objects. This is a
       collection of behaviors that don't necessarily imply a Perl-level
       protocol, but which may end up meaning something at some point.]);
+
+ni->extend("src/semantic/$_") for
+  qw/ dimension
+      task /;
