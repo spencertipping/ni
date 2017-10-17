a00081628d84a04ed5fb5dbe991ec44661a3b630
Michael Bilow <bilow@factual.com>
2017-10-17 06:29:12 -0700

    added json delete utils

--- core/pl/json_util.pm
+++ core/pl/json_util.pm
@@ -18,3 +18,39 @@ sub get_flat_hash {
   return @raw_data;
 }
 
+sub join_json_parts {
+  if (substr($_[1], 0, 1) eq ",") {
+    return join '', $_[0], substr($_[1], 1); 
+  } elsif ($_[1] eq "}" and substr($_[0], -1) eq ",") {
+    return join '', substr($_[0], 0, -1), $_[1];
+  }  else {
+    return join '', @_;
+  }
+}
+
+sub delete_array {
+  my @raw_data = $_[0] =~ /^(.*[{,])"$_[1]":\[[^]]+\]([},].*)$/;
+  if(@raw_data) {
+    return join_json_parts @raw_data;
+  } else {
+    return $_[0];
+  }
+}
+
+sub delete_scalar {
+  my @raw_data = $_[0] =~ /^(.*[{,])"$_[1]":(?:"[^"]*"|-?\d+.?\d*)([},].*)$/;
+  if(@raw_data) {
+    return join_json_parts @raw_data;
+  } else {
+    return $_[0];
+  }
+}
+
+sub delete_flat_hash {
+  my @raw_data = $_[0] =~ /^(.*[{,])"$_[1]":{[^}]*}([},].*)$/;
+  if(@raw_data) {
+    return join_json_parts @raw_data;
+  } else {
+    return $_[0];
+  }
+}
