aa7bba0f6210ca470c4b6532f9b3a0db0ae84562
Michael Bilow <bilow@factual.com>
2017-12-25 18:06:59 +0100

    added shell quote

--- core/hadoop/hdfsjoin.pl
+++ core/hadoop/hdfsjoin.pl
@@ -35,7 +35,7 @@ defresource 'hdfsc',
                     my $n_files = $_[1];
                     my $shift_amt = $n_files == 10 ? 1 : $n_files == 100 ? 2 : die "only 10 or 100 files";
                     my $stem_path = stem_partfile_path $map_fn, $shift_amt; 
-                    my $compact_path = join "/", $map_folder, $stem_path;
+                    my $compact_path = shell_quote join "/", $map_folder, $stem_path;
                     sh qq{$hadoop_name fs -text $compact_path 2>/dev/null }} @_};
  
 defresource 'hdfscname',
