643d9186d38673c9bb234c11069269c61d9471f2
Spencer Tipping <spencer.tipping@gmail.com>
2018-12-13 01:01:15 -0700

    And heck, let's live large and do md5_base64 while we're at it

--- core/bloom/bloomfilter.pl
+++ core/bloom/bloomfilter.pl
@@ -1,7 +1,7 @@
 # Bloom filter library.
 # A simple pure-Perl implementation of Bloom filters.
 
-eval {require Digest::MD5; Digest::MD5->import('md5', 'md5_hex')};
+eval {require Digest::MD5; Digest::MD5->import(qw/md5 md5_hex md5_base64/)};
 
 # Swiped from https://hur.st/bloomfilter
 sub bloom_args($$) {
